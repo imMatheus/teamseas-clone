634943bfcad1e39a10c0b5133da36ada6b143a6f
imMatheus <matheusmendesbarata03@gmail.com>
2022-03-13 18:44:55 +0100

    added prisma

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { DonationsModule } from './donations/donations.module';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLDateTime } from 'graphql-iso-date';
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      playground: false,
+      driver: ApolloDriver,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      typePaths: ['./**/*.graphql'],
+      resolvers: { DateTime: GraphQLDateTime },
+    }),
+    DonationsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
